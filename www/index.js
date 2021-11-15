bc172612bbd4b6de98e35706987571ccccffa1fc
vcx100g <vcx100g@gmail.com>
2021-11-15 16:26:50 +0800

    time profiling

--- www/index.js
+++ www/index.js
@@ -59,7 +59,45 @@ playPauseButton.addEventListener("click", event => {
     } else {
         pause();
     }
-})
+});
+
+const fps = new class {
+    constructor() {
+        this.fps = document.getElementById("fps");
+        this.frames = [];
+        this.lastFrameTimeStamp = performance.now();
+    }
+
+    render() {
+        const now = performance.now();
+        const delta = now - this.lastFrameTimeStamp;
+        this.lastFrameTimeStamp = now;
+        const fps = 1 / delta * 1000;
+
+        this.frames.push(fps);
+        if (this.frames.length > 100) {
+            this.frames.shift();
+        }
+
+        let min = Infinity;
+        let max = Infinity;
+        let sum = 0;
+        for (let i = 0; i < this.frames.length; i++) {
+            sum += this.frames[i];
+            min = Math.min(this.frames[i], min);
+            max = Math.max(this.frames[i], max);
+        }
+        let mean = sum / this.frames.length;
+
+        this.fps.textContent = `
+Frames per Second:
+         latest = ${Math.round(fps)}
+avg of last 100 = ${Math.round(mean)}
+min of last 100 = ${Math.round(min)}
+max of last 100 = ${Math.round(max)}
+`.trim();
+    }
+}
 
 
 const drawGrid = () => {
@@ -91,11 +129,30 @@ const drawCells = () => {
 
     ctx.beginPath();
 
+    // for (let row = 0; row < height; row++) {
+    //     for (let col = 0; col < width; col++) {
+    //         const idx = getIndex(row, col);
+    //
+    //         ctx.fillStyle = cells[idx] === Cell.Dead ? DEAD_COLOR : ALIVE_COLOR;
+    //
+    //         ctx.fillRect(
+    //             col * (CELL_SIZE + 1) + 1,
+    //             row * (CELL_SIZE + 1) + 1,
+    //             CELL_SIZE,
+    //             CELL_SIZE
+    //         );
+    //     }
+    // }
+
+    // big O speed up for fillStyle
+    // Alive cells
+    ctx.fillStyle = ALIVE_COLOR;
     for (let row = 0; row < height; row++) {
         for (let col = 0; col < width; col++) {
             const idx = getIndex(row, col);
-
-            ctx.fillStyle = cells[idx] === Cell.Dead ? DEAD_COLOR : ALIVE_COLOR;
+            if (cells[idx] !== Cell.Alive) {
+                continue;
+            }
 
             ctx.fillRect(
                 col * (CELL_SIZE + 1) + 1,
@@ -106,12 +163,47 @@ const drawCells = () => {
         }
     }
 
+    // Dead cells
+    ctx.fillStyle = DEAD_COLOR;
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            const idx = getIndex(row, col);
+            if (cells[idx] !== Cell.Dead) {
+                continue;
+            }
+
+            ctx.fillRect(
+                col * (CELL_SIZE + 1) + 1,
+                row * (CELL_SIZE + 1) + 1,
+                CELL_SIZE,
+                CELL_SIZE
+            );
+        }
+    }
+
+
     ctx.stroke();
 }
 
 const renderLoop = () => {
-    debugger;
-    universe.tick();
+    // debugger;
+
+    // add set time out so browser dev tool faster
+    // setTimeout(() => {
+    //     fps.render();
+    //     universe.tick();
+    //
+    //     drawGrid();
+    //     drawCells();
+    //
+    //     animationId = requestAnimationFrame(renderLoop);
+    // }, 200);
+
+    fps.render();
+    // universe.tick();
+    for (let i = 0; i < 9; i++) {
+        universe.tick();
+    }
 
     drawGrid();
     drawCells();
